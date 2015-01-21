31073b9c548ab90d437788ea651ae994ee42902d
Riley Doyle <rileyd@desktopgenetics.com>
2015-01-21 17:00:50 +0000

    Added .gitignore file

--- brkpoints/main.py
+++
@@ -1,9 +0,0 @@
-'This is a module docstring. It describes the functions on offer.'
-
-
-def main():
-    'This is a function docstring. It describes the behaviour of the function.'
-    print('MAIN FUNCTION CALLED')
-
-if __name__ == '__main__':
-    main()
