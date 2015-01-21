73914a5d077049d1a605521fa296391cbdc0a178
Riley Doyle <rileyd@desktopgenetics.com>
2015-01-21 17:02:18 +0000

    added dependencies to setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ config = {
     'download_url': 'Where to download it.',
     'author_email': 'My email.',
     'version': '0.1',
-    'install_requires': ['nose'],
+    'install_requires': ['pytest', 'biopython'],
     'packages': ['brkpoints'],
     'scripts': [],
     'name': 'projectname'
